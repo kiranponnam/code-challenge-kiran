ff3cbe839f36eefd75601759e823ef31e00c0286
Kiran Ponnam <62033569+kiranponnam@users.noreply.github.com>
2025-01-08 18:09:52 +0530

    task-2

--- project/packages/ui/components/List.tsx
+++ project/packages/ui/components/List.tsx
@@ -1,5 +1,28 @@
-import React from 'react'
+import React from "react";
 
-export const List = () => {
-  return <p>List</p>
+interface Pokemon {
+  name: string;
+  url: string;
 }
+interface ListProps {
+  pokemon: Pokemon[];
+  isLoading: boolean;
+  error: string | null;
+}
+
+export const List: React.FC<ListProps> = ({ pokemon, isLoading, error }) => {
+  return (
+    <>
+      {isLoading && <p>Loading...</p>}
+       {error && <p>{error}</p>}
+      <div className="pokemon-main-container">
+        {pokemon?.map((item: Pokemon, index: number) => (
+          <div key={index} className="pokemon-card" tabIndex={0}>
+            <p className="pokemon-name">{item?.name}</p>
+            <p>{item?.url}</p>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
