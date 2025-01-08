be73e1e63740b6adbc06c3be540e79b2037458ea
Kiran Ponnam <62033569+kiranponnam@users.noreply.github.com>
2025-01-08 18:28:58 +0530

    task-5

--- project/packages/ui/components/List.tsx
+++ project/packages/ui/components/List.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "./List.css";
 
 interface Pokemon {
   name: string;
@@ -9,18 +8,25 @@ interface ListProps {
   pokemon: Pokemon[];
   isLoading: boolean;
   error: string | null;
+  handleDelete:()=>void;
 }
 
-export const List: React.FC<ListProps> = ({ pokemon, isLoading, error }) => {
+export const List: React.FC<ListProps> = ({ pokemon, isLoading, error,handleDelete }) => {
   return (
     <>
       {isLoading && <p>Loading...</p>}
-       {error && <p>{error}</p>}
+      {error && <p>{error}</p>}
       <div className="pokemon-main-container">
         {pokemon?.map((item: Pokemon, index: number) => (
           <div key={index} className="pokemon-card" tabIndex={0}>
             <p className="pokemon-name">{item?.name}</p>
             <p>{item?.url}</p>
+            <button
+              className="pokemon-button"
+              onClick={() => handleDelete(index)}
+            >
+              Delete
+            </button>
           </div>
         ))}
       </div>
