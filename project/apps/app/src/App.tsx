a242d0cfb3cf9eaf7a851a434d95c9fd5c7aa8e8
Kiran Ponnam <62033569+kiranponnam@users.noreply.github.com>
2025-01-08 18:05:56 +0530

    task-1

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -1,12 +1,41 @@
-import { List } from 'ui'
+import { List } from 'ui';
+import { useEffect, useState } from "react";
 
-const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
-
+const API_URL = "https://pokeapi.co/api/v2/pokemon?limit=100";
+interface Pokemon {
+  name: string;
+  url: string;
+}
 const App = () => {
+  const [isLoading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const [pokemonList, setPokemonList] = useState<Pokemon[]>([]);
+
+   const fetchData = async () => {
+    try {
+      setLoading(true);
+      setError(null); 
+      const response = await fetch(API_URL);
+      if (!response.ok)
+        throw new Error(`Failed to fetch: ${response.statusText}`);
+      const data = await response.json();
+      setPokemonList(data)
+      dispatch(setPokemonData(data.results));
+    } catch (error: any) {
+      setError(error.message || "An unexpected error occurred.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+  
   return (
   <>
     <h1>Pokemon list:</h1>
-    <List />
+    <List pokemon={pokemonList} />
   </>
   )
 }
