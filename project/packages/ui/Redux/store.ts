20c72ea99810b82767c724672cad2d133a1e6462
Kiran Ponnam <62033569+kiranponnam@users.noreply.github.com>
2025-01-08 18:21:55 +0530

    Delete project/packages/ui/Redux directory and add to apps

--- project/packages/ui/Redux/store.ts
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import pokemonReducer from './Pokeman/PokemanSlice';
-
-export const store = configureStore({
-  reducer: {
-    pokeman: pokemonReducer,
-  },
-});
-export type RootState = ReturnType<typeof store.getState>;
